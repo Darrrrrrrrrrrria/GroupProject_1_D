42effea70476f13c746249f9269cb6957dab33fc
Darrrrrrrrrrrria <161242102+Darrrrrrrrrrrria@users.noreply.github.com>
2024-03-25 15:27:52 +0300

    Update Num_2.cpp

--- Num_2/Num_2.cpp
+++ Num_2/Num_2.cpp
@@ -2,12 +2,10 @@
 using namespace std;
 int main() {
 	int q, n, i=0, j, s, p, fact = 1, sum = 0, r = 0, e = 0;
-	//cout << "n=";
 	cin >> n;
 	q = n * n;
 	int** m;
 	m = new int* [q];
-	//cout << "Vvedi matr:\n";
 	//Ввод матрицы
 	for (i = 0; i < n; i++)
 	{
@@ -15,15 +13,6 @@ int main() {
 		for (j = 0; j < n; j++)
 			cin >> m[i][j];
 	}
-	//cout << endl << "Matr:";
-	//Вывод матрицы
-	//for (i = 0; i < n; i++)
-	//{
-	//	cout << endl;
-	//	for (j = 0; j < n; j++)
-	//		cout << " " << m[i][j];
-	//}
-	//cout << endl;
 	i = 0;
 	//Подсчет суммы и факториала
 	while (i != n)
